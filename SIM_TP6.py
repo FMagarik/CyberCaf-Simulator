94a9eba0ffb62592e0815d85c9b2dc7d82164faa
Facundo E. Lopez Magarik <facu.emiliano@gmail.com>
2019-05-15 01:37:50 -0300

    Removed unnecesary variables, implemented pdfs

--- SIM_TP6.py
+++ SIM_TP6.py
@@ -1,16 +1,17 @@
 from random import random
 from random import randint
+from numpy.random import weibull
 
 tf = 100000
 
 def printCols(row):
-	print("{: >10} | {: >10} | {: >10} | {: >10} | {: >10} | {: >10} | {: >10} | {: >10}".format(*row))
+	print("{: >10} | {: >10} | {: >10} | {: >10} | {: >10} | {: >10}".format(*row))
 
 def perc(v):
 	return(str(v) + '%')
 
 def sim(d, l):
-	t = tpll = npl = npd = npa15 = stol = stod = sec = stsl = stsd = 0
+	t = tpll = npl = npd = npa15 = stol = stod = sec = 0
 	tcd = [0] * d
 	tcl = [0] * l
 
@@ -18,10 +19,12 @@ def sim(d, l):
 		return randint(5, 15)
 
 	def tal():
-		return randint(70, 110)
+		a, B = 0.96357, 0.05201  # scale , shape
+		s = (weibull(a, 1) * B)[0]
+		return int(round(s * 24 * 60))
 
 	def tad():
-		return randint(15, 60)
+		return round(-0.7102 * 24 * 60 * ((1 - random()) ** 0.3179919075296878 - 1.01179))
 
 	def esTiempoLibre():
 		return random() <= 0.5562
@@ -41,7 +44,6 @@ def sim(d, l):
 			else:
 				sec += tcl[i] - t
 				tcl[i] += tal()
-			stsl += tcl[i] - t
 			npl += 1
 		else:
 			i = tcd.index(min(tcd))
@@ -54,20 +56,17 @@ def sim(d, l):
 				else:
 					sec += tcd[i] - t
 					tcd[i] += tad()
-				stsd += tcd[i] - t
 				npd += 1
 
 		if t > tf:
 			ptod = format(100 * stod / (d * t))
 			ptol = format(100 * stol / (l * t))
 			ppa15 = format(100 * npa15 / (npa15 + npl + npd))
-			ppsd = format(stsd / npd)
-			ppsl = format(stsl / npl)
 			pec = format(sec / (npl + npd))
-			return [d, l, perc(ptod), perc(ptol), perc(ppa15), ppsd, ppsl, pec]
+			return [d, l, perc(ptod), perc(ptol), perc(ppa15), pec]
 
 def simPCs(max):
-	printCols(['d', 'l', 'ptod', 'ptol', 'ppa15', 'ppsd', 'ppsl', 'pec'])
+	printCols(['d', 'l', 'ptod', 'ptol', 'ppa15', 'pec'])
 	d = 1
 	while d < max:
 		l = max - d
@@ -76,4 +75,4 @@ def simPCs(max):
 		d += 1
 	print()
 
-simPCs(9)
+simPCs(20)
